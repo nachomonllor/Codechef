b0c975bd738fa2ad29b885be36e2cdd9ed2d4db9
nachomonllor <nachomonllorc@gmail.com>
2019-11-26 18:21:04 -0300

    agrego4

---
+++ Chef and Chain.cs
@@ -0,0 +1,51 @@
+﻿https://www.codechef.com/problems/CHEFCH
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int Count(string s)
+{
+    
+
+    string a = "";
+    string b = "";
+    for (int i = 0; i < s.length() / 2 + 1; i++)
+    {
+        a += "-+";
+        b += "+-";
+    }
+
+    int conta = 0, contb = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != a[i])
+        {
+            conta++;
+        }
+        if (s[i] != b[i])
+        {
+            contb++;
+        }
+    }
+    return min(conta, contb);
+}
+
+int main()
+{
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        string s ;
+        cin >> s;
+        cout << Count(s) << endl;
+    }
+   
+	return 0;
+}
