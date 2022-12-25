170eb0f4cefb72ba8cc88e94b988ed5ce23240d8
nachomonllor <nachomonllorc@gmail.com>
2022-12-25 19:16:58 -0300

    Coins and triangle (con busqueda binaria)

--- Coins And Triangle.cs
+++ Coins And Triangle.cs
@@ -1,4 +1,4 @@
-﻿https://www.codechef.com/problems/TRICOIN
+﻿//https://www.codechef.com/problems/TRICOIN
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -48,6 +48,89 @@ namespace ConsoleApplication1
         }
 
 
+    }
+}
+
+//------------------------------Con busqueda Binaria (año 2022) --------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+
+        static int BusquedaBinaria(List<int> pascal, int buscado)
+        {
+            int left = 0, rigth = pascal.Count - 1;
+
+            while (left <= rigth)
+            {
+                int middle = left + (rigth - left) /2;
+
+                if (buscado >= pascal[middle] && buscado <= pascal[middle + 1]) 
+                {
+                    return middle + 1;
+                }
+                else if (pascal[middle] < buscado)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    rigth = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> pascal = new List<int>();
+
+                int s = 0;
+                for (int i = 1; s< Math.Pow(10, 9); i++)
+                {
+                    s += i;
+                    pascal.Add(s);
+                }
+
+                int T = int.Parse(Console.ReadLine().Trim());
+                List<int> test = new List<int>();
+                for (int i = 0; i < T; i++)
+                {
+                    test.Add(int.Parse(Console.ReadLine().Trim()));
+                }
+
+
+                StringBuilder sb = new StringBuilder();
+
+                //test = 3 5 7
+                //ans =  2 2 3
+
+                for (int i = 0; i < test.Count; i++)
+                {
+                    sb.AppendLine(BusquedaBinaria(pascal, test[i]) + "");
+                }
+
+                Console.Write(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+
+        }
     }
 }
 
